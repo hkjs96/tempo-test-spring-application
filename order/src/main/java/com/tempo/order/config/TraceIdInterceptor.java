9cf45088a6d9b473edbee50f1ff10863046f3758
bakjisu <tlsgks678@mz.co.kr>
2025-01-22 17:44:09 +0900

    add logging for loki

---
+++ order/src/main/java/com/tempo/order/config/TraceIdInterceptor.java
@@ -0,0 +1,32 @@
+package com.tempo.order.config;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class TraceIdInterceptor implements HandlerInterceptor {
+    private static final Tracer tracer = GlobalOpenTelemetry.getTracer("my-app");
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Span span =  Span.current();
+        if(span != null) {
+            String traceId = span.getSpanContext().getTraceId();
+            MDC.put("traceId", traceId);
+        }
+        return true;
+    }
+
+//    @Override
+//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        MDC.remove("traceId");
+    }
+}
