2fb846ec9b0c468c32d5fbf7aab590e1e2273999
bakjisu <tlsgks678@mz.co.kr>
2025-02-05 16:16:57 +0900

    feature trace to matric check test

--- order/src/main/java/com/tempo/order/controller/OrderErrorTestController.java
+++ order/src/main/java/com/tempo/order/controller/OrderErrorTestController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
@@ -13,7 +14,7 @@ public class OrderErrorTestController {
 
     // Order 서비스의 에러 발생 엔드포인트
     @GetMapping("/error")
-    public ResponseEntity<String> triggerOrderError() {
-        throw new RuntimeException("Order 서비스 내부 오류 발생");
+    public Mono<ResponseEntity<String>> triggerOrderError() {
+        return Mono.error(new RuntimeException("Order 서비스 내부 오류 발생"));
     }
 }
